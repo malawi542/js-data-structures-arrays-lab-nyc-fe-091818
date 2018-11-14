c5d799b1d3665e6fe00145263a2c83cf7393eb71
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-11-14 01:03:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+  drivers.push('Ralph')
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift('Bob')
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift()
+}
+
+function appendDriver(name) {
+  const driversCopy = drivers.slice();
+  driversCopy.push("Broom")
+  console.log(driversCopy)
+  console.log(drivers)
+}
+
+function prependDriver(name){
+  const driversCopy = drivers.slice();
+  driversCopy.unshift("Arnold")
+  console.log(driversCopy)
+  console.log(drivers)
+}
+
---
+++ index2.js
@@ -0,0 +1,16 @@
+function destructivelyAppendDriver(Name) {
+  drivers.shift()
+}
+
+function destructivelyAppendDriver(Name) {
+  drivers.pop()
+}
+
+function destructivelyAppendDriver(Name) {
+  drivers.shift()
+}
+
+function appendDriver(Name) {
+  const driversCopy = drivers
+}
+
